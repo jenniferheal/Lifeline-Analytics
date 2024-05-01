2e864ffc3fb8b6221f7e65b217b55a6eebba7269
Alan Ruanova <ruanovaalan96@gmail.com>
2024-05-01 17:00:39 -0600

    Add 404 error handling in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res) => {
+  res.send('<h1>404</h1>')
+})
+
 app.listen(
   5000,
   () => console.log('listening in port 5000')
