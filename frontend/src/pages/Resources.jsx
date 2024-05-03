fd7a370d7554291902fa3e5e0825c76e2fba2f03
Alan Ruanova <ruanovaalan96@gmail.com>
2024-05-03 11:56:12 -0600

    Add new pages and update navigation links

---
+++ frontend/src/pages/Resources.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Resources() {
+  return (
+    <>
+      <Navbar />
+      <h1>Resources</h1>
+      <Footer />
+    </>
+  )
+}
