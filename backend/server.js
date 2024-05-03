f4e687a7d5dfe865fd924b33afa2a31d74af056c
jenniferheal <jennifer.alv02@gmail.com>
2024-05-03 16:23:30 -0600

    Added cookie configuration with cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+
 const suicideRoutes = require('./src/SUICIDE_ANALYSIS/routes')
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
