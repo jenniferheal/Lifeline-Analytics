91ba167733d81ab47b2401d3e0d360a7a52772b9
jenniferheal <jennifer.alv02@gmail.com>
2024-05-03 13:28:23 -0600

    Update db.js to use environment variables from .env

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'SUICIDE_ANALYSIS',
-  password: '',
-  port: 5432
-})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT, 10)
+});
 
-module.exports = pool
+module.exports = pool;
