7ff4bf3d5c20ea278b4f185fbe0cd828033acc13
jenniferheal <jennifer.alv02@gmail.com>
2024-05-03 13:31:00 -0600

    Add getAllResources and getOneResource functionalities

--- backend/src/SUICIDE_ANALYSIS/queries.js
+++ backend/src/SUICIDE_ANALYSIS/queries.js
@@ -1,5 +1,22 @@
-const getSuicides = 'SELECT * FROM suicides where id = 127465'
+// queries.js
+const getSuicides = 'SELECT * FROM suicides';
+
+const getAllResourcesQuery = `
+  SELECT resources.id, resources.title, resources.description, resources.link, resources.id_country, country.name AS country_name
+  FROM resources
+  JOIN country ON resources.id_country = country.id;
+`;
+
+const getOneResourceQuery = `
+  SELECT resources.id, resources.title, resources.description, resources.link, resources.id_country, country.name AS country_name
+  FROM resources
+  JOIN country ON resources.id_country = country.id
+  WHERE resources.id = $1;
+`;
 
 module.exports = {
-  getSuicides
-}
+  getSuicides,
+  getAllResourcesQuery,
+  getOneResourceQuery
+};
+
--- backend/src/SUICIDE_ANALYSIS/routes.js
+++ backend/src/SUICIDE_ANALYSIS/routes.js
@@ -4,5 +4,7 @@ const controller = require('./controller')
 const router = Router()
 
 router.get('/', controller.getSuicides)
+router.get('/resources', controller.getAllResources);
+router.get('/resources/:id', controller.getOneResource);
 
 module.exports = router
