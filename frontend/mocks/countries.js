8ba813ff6058ab8c729dcaaaac525a29b3f203a4
Alan Ruanova <ruanovaalan96@gmail.com>
2024-05-03 20:30:32 -0600

    Add smart select to signup and add countries.js file

---
+++ frontend/mocks/countries.js
@@ -0,0 +1,472 @@
+export const countries = [
+  {
+    value: 'ALB',
+    label: 'Albania'
+  },
+  {
+    value: 'ATG',
+    label: 'Antigua and Barbuda'
+  },
+  {
+    value: 'ARG',
+    label: 'Argentina'
+  },
+  {
+    value: 'ARM',
+    label: 'Armenia'
+  },
+  {
+    value: 'AUS',
+    label: 'Australia'
+  },
+  {
+    value: 'AUT',
+    label: 'Austria'
+  },
+  {
+    value: 'AZE',
+    label: 'Azerbaijan'
+  },
+  {
+    value: 'BHS',
+    label: 'Bahamas'
+  },
+  {
+    value: 'BHR',
+    label: 'Bahrain'
+  },
+  {
+    value: 'BRB',
+    label: 'Barbados'
+  },
+  {
+    value: 'BLR',
+    label: 'Belarus'
+  },
+  {
+    value: 'BEL',
+    label: 'Belgium'
+  },
+  {
+    value: 'BLZ',
+    label: 'Belize'
+  },
+  {
+    value: 'BIH',
+    label: 'Bosnia and Herzegovina'
+  },
+  {
+    value: 'BRA',
+    label: 'Brazil'
+  },
+  {
+    value: 'BRN',
+    label: 'Brunei Darussalam'
+  },
+  {
+    value: 'BGR',
+    label: 'Bulgaria'
+  },
+  {
+    value: 'CPV',
+    label: 'Cabo Verde'
+  },
+  {
+    value: 'CAN',
+    label: 'Canada'
+  },
+  {
+    value: 'CHL',
+    label: 'Chile'
+  },
+  {
+    value: 'COL',
+    label: 'Colombia'
+  },
+  {
+    value: 'CRI',
+    label: 'Costa Rica'
+  },
+  {
+    value: 'HRV',
+    label: 'Croatia'
+  },
+  {
+    value: 'CUB',
+    label: 'Cuba'
+  },
+  {
+    value: 'CYP',
+    label: 'Cyprus'
+  },
+  {
+    value: 'CZE',
+    label: 'Czechia'
+  },
+  {
+    value: 'DNK',
+    label: 'Denmark'
+  },
+  {
+    value: 'DMA',
+    label: 'Dominica'
+  },
+  {
+    value: 'DOM',
+    label: 'Dominican Republic'
+  },
+  {
+    value: 'ECU',
+    label: 'Ecuador'
+  },
+  {
+    value: 'EGY',
+    label: 'Egypt'
+  },
+  {
+    value: 'SLV',
+    label: 'El Salvador'
+  },
+  {
+    value: 'EST',
+    label: 'Estonia'
+  },
+  {
+    value: 'FJI',
+    label: 'Fiji'
+  },
+  {
+    value: 'FIN',
+    label: 'Finland'
+  },
+  {
+    value: 'FRA',
+    label: 'France'
+  },
+  {
+    value: 'GUF',
+    label: 'French Guiana'
+  },
+  {
+    value: 'GEO',
+    label: 'Georgia'
+  },
+  {
+    value: 'DEU',
+    label: 'Germany'
+  },
+  {
+    value: 'GRC',
+    label: 'Greece'
+  },
+  {
+    value: 'GRD',
+    label: 'Grenada'
+  },
+  {
+    value: 'GLP',
+    label: 'Guadeloupe'
+  },
+  {
+    value: 'GTM',
+    label: 'Guatemala'
+  },
+  {
+    value: 'GUY',
+    label: 'Guyana'
+  },
+  {
+    value: 'HKG',
+    label: 'China, Hong Kong SAR'
+  },
+  {
+    value: 'HUN',
+    label: 'Hungary'
+  },
+  {
+    value: 'ISL',
+    label: 'Iceland'
+  },
+  {
+    value: 'IRN',
+    label: 'Iran (Islamic Republic of)'
+  },
+  {
+    value: 'IRQ',
+    label: 'Iraq'
+  },
+  {
+    value: 'IRL',
+    label: 'Ireland'
+  },
+  {
+    value: 'ISR',
+    label: 'Israel'
+  },
+  {
+    value: 'ITA',
+    label: 'Italy'
+  },
+  {
+    value: 'JAM',
+    label: 'Jamaica'
+  },
+  {
+    value: 'JPN',
+    label: 'Japan'
+  },
+  {
+    value: 'KAZ',
+    label: 'Kazakhstan'
+  },
+  {
+    value: 'KOR',
+    label: 'Republic of Korea'
+  },
+  {
+    value: 'KWT',
+    label: 'Kuwait'
+  },
+  {
+    value: 'KGZ',
+    label: 'Kyrgyzstan'
+  },
+  {
+    value: 'LVA',
+    label: 'Latvia'
+  },
+  {
+    value: 'LTU',
+    label: 'Lithuania'
+  },
+  {
+    value: 'LUX',
+    label: 'Luxembourg'
+  },
+  {
+    value: 'MDV',
+    label: 'Maldives'
+  },
+  {
+    value: 'MLT',
+    label: 'Malta'
+  },
+  {
+    value: 'MTQ',
+    label: 'Martinique'
+  },
+  {
+    value: 'MUS',
+    label: 'Mauritius'
+  },
+  {
+    value: 'MYT',
+    label: 'Mayotte'
+  },
+  {
+    value: 'MEX',
+    label: 'Mexico'
+  },
+  {
+    value: 'MDA',
+    label: 'Republic of Moldova'
+  },
+  {
+    value: 'MNE',
+    label: 'Montenegro'
+  },
+  {
+    value: 'NLD',
+    label: 'Netherlands'
+  },
+  {
+    value: 'NZL',
+    label: 'New Zealand'
+  },
+  {
+    value: 'NIC',
+    label: 'Nicaragua'
+  },
+  {
+    value: 'MKD',
+    label: 'North Macedonia'
+  },
+  {
+    value: 'NOR',
+    label: 'Norway'
+  },
+  {
+    value: 'PAN',
+    label: 'Panama'
+  },
+  {
+    value: 'PRY',
+    label: 'Paraguay'
+  },
+  {
+    value: 'PER',
+    label: 'Peru'
+  },
+  {
+    value: 'PHL',
+    label: 'Philippines'
+  },
+  {
+    value: 'POL',
+    label: 'Poland'
+  },
+  {
+    value: 'PRT',
+    label: 'Portugal'
+  },
+  {
+    value: 'PRI',
+    label: 'Puerto Rico'
+  },
+  {
+    value: 'REU',
+    label: 'R?union'
+  },
+  {
+    value: 'ROU',
+    label: 'Romania'
+  },
+  {
+    value: 'RUS',
+    label: 'Russian Federation'
+  },
+  {
+    value: 'KNA',
+    label: 'Saint Kitts and Nevis'
+  },
+  {
+    value: 'LCA',
+    label: 'Saint Lucia'
+  },
+  {
+    value: 'VCT',
+    label: 'Saint Vincent and the Grenadines'
+  },
+  {
+    value: 'SRB',
+    label: 'Serbia'
+  },
+  {
+    value: 'SYC',
+    label: 'Seychelles'
+  },
+  {
+    value: 'SGP',
+    label: 'Singapore'
+  },
+  {
+    value: 'SVK',
+    label: 'Slovakia'
+  },
+  {
+    value: 'SVN',
+    label: 'Slovenia'
+  },
+  {
+    value: 'ZAF',
+    label: 'South Africa'
+  },
+  {
+    value: 'ESP',
+    label: 'Spain'
+  },
+  {
+    value: 'LKA',
+    label: 'Sri Lanka'
+  },
+  {
+    value: 'SUR',
+    label: 'Suriname'
+  },
+  {
+    value: 'SWE',
+    label: 'Sweden'
+  },
+  {
+    value: 'CHE',
+    label: 'Switzerland'
+  },
+  {
+    value: 'SYR',
+    label: 'Syrian Arab Republic'
+  },
+  {
+    value: 'TJK',
+    label: 'Tajikistan'
+  },
+  {
+    value: 'THA',
+    label: 'Thailand'
+  },
+  {
+    value: 'TTO',
+    label: 'Trinidad and Tobago'
+  },
+  {
+    value: 'TUR',
+    label: 'T?rkiye'
+  },
+  {
+    value: 'TKM',
+    label: 'Turkmenistan'
+  },
+  {
+    value: 'UKR',
+    label: 'Ukraine'
+  },
+  {
+    value: 'GBR',
+    label: 'United Kingdom of Great Britain and Northern Ireland'
+  },
+  {
+    value: 'USA',
+    label: 'United States of America'
+  },
+  {
+    value: 'URY',
+    label: 'Uruguay'
+  },
+  {
+    value: 'UZB',
+    label: 'Uzbekistan'
+  },
+  {
+    value: 'VEN',
+    label: 'Venezuela (Bolivarian Republic of)'
+  },
+  {
+    value: 'JOR',
+    label: 'Jordan'
+  },
+  {
+    value: 'ARE',
+    label: 'United Arab Emirates'
+  },
+  {
+    value: 'LBN',
+    label: 'Lebanon'
+  },
+  {
+    value: 'MNG',
+    label: 'Mongolia'
+  },
+  {
+    value: 'MYS',
+    label: 'Malaysia'
+  },
+  {
+    value: 'OMN',
+    label: 'Oman'
+  },
+  {
+    value: 'QAT',
+    label: 'Qatar'
+  }
+]
+
+export const countriesForResources = [...countries, { value: 'ALL', label: 'All Countries' }, { value: 'WORLD', label: 'Global Aid' }]
